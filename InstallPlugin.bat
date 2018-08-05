9566983b2cebacc053cb602ffa074ccd291e76b3
masjinno <masjinno@gmail.com>
2018-08-06 02:27:44 +0900

    Update InstallPlugin.bat

--- InstallPlugin.bat
+++ InstallPlugin.bat
@@ -1,1 +1,10 @@
+@echo off
+pushd "%~dp0"
+
+set installCommand=adb install -r ./app/build/outputs/apk/debug/app-debug.apk
+echo ^>%installCommand%
 adb install -r ./app/build/outputs/apk/debug/app-debug.apk
+echo %ERRORLEVEL%
+
+popd
+exit /b %ERRORLEVEL%
